dfc5f3991226387dac0916e4045d777fb88ea1b3
kellycanela <kellycanela919@gmail.com>
2026-01-03 00:17:58 +0100

    page contact terminée

--- pages/contact.php
+++ pages/contact.php
@@ -8,8 +8,28 @@ require('../templates/header.php');
         <p class="lead">Réservez votre table ou contactez-nous pour toute question</p><br><br>
     </div>
 </header>
+<h2 class="text-center my-5 fw-bolder"> Informations pratiques</h2>
 
-
+<table class="table">
+  <tbody>
+    <tr>
+      <th width="4px"><img src="/site_restaurant/assets/icons/adresse.png" alt="adresse" width="40"></th>
+      <td><h5>Adresse</h5>Bonamoussadi, Douala</td>
+    </tr>
+    <tr>
+      <th ><img src="/site_restaurant/assets/icons/telephone.png" alt="telephone" width="25"></th>
+      <td><h5>Téléphone</h5> +237 696 69 87 25</td>
+    </tr>
+    <tr>
+      <th ><img src="/site_restaurant/assets/icons/mail.png" alt="email" width="25"></th>
+      <td><h5>Email</h5> kellycanela919@gmail.com</td>
+    </tr>
+    <tr>
+      <th ><img src="/site_restaurant/assets/icons/clock.png" alt="clock" width="25"></th>
+      <td><h5>Horaire d'ouverture</h5> <div display:flex;> <div style="color:blue;">Lundi - Vendredi:</div> 12h00 - 14h30, 19h00 - 22h30 </div><div display:flex;><div style="color:blue;">Samedi:</div> 19h00 - 23h00 </div><div display:flex;><div style="color:blue;">Dimanche:</div>Fermé</div></td>
+    </tr>
+  </tbody>
+</table>
 
 <?php 
 require('../templates/footer.php');
